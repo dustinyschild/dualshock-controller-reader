be41038ca1368aa3328ff54141d482a0f77bf06d
dustinyschild <dyelland@gmail.com>
2017-10-19 15:17:13 -0500

    controller is working on pi!

--- controller.js
+++ controller.js
@@ -5,7 +5,7 @@ var dualShock = require('dualshock-controller');
 var controller = dualShock(
   {
     //you can use a ds4 by uncommenting this line.
-    config: 'dualshock4-alternate-driver',
+    config: 'dualShock4-alternate-driver',
     //if using ds4 comment this line.
     //config : "dualShock3",
     //smooths the output from the acelerometers (moving averages) defaults to true
