cf36e4ebc2afeb86ed3cc66074f3e7f80a3635a5
Dustin <dyelland@gmail.com>
2017-10-19 15:22:29 -0500

    test gpio with controller

--- actions/index.js
+++ actions/index.js
@@ -1,7 +1,18 @@
+const onoff = require('onoff');
+const Gpio = onoff.Gpio;
+
 const actions = {};
+
+const pin23 = new Gpio(23,'out');
 
 actions.circlePress = () => {
   console.log('circle button pressed');
+  pin23.write(1);
+};
+
+actions.circleRelease = () => {
+  console.log('circle button released');
+  pin23.write(0);
 };
 
 
